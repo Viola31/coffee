85011201162e834149f76aa735feab78a20645c9
Viola31 <violainkie@gmail.com>
2025-05-26 21:11:45 +0300

    lab8

--- scriptlocation.js
+++ scriptlocation.js
@@ -1,31 +1,39 @@
 let draggedElement = null;
 let offsetX = 0;
 let offsetY = 0;
-document.querySelectorAll('.location-img').forEach(img => {
+document.querySelectorAll('.location-img').forEach(function (img) {
     img.style.position = 'relative';
     img.style.cursor = 'grab';
-    img.addEventListener('mousedown', function (e) {
-        draggedElement = e.target;
-        offsetX = e.clientX - draggedElement.getBoundingClientRect().left;
-        offsetY = e.clientY - draggedElement.getBoundingClientRect().top;
-        draggedElement.style.position = 'absolute';
-        draggedElement.style.zIndex = 1000;
-        draggedElement.style.cursor = 'grabbing';
-        document.body.appendChild(draggedElement);
-        moveAt(e.pageX, e.pageY);
-        function moveAt(pageX, pageY) {
-            draggedElement.style.left = pageX - offsetX + 'px';
-            draggedElement.style.top = pageY - offsetY + 'px';
-        }
-        function onMouseMove(e) {
-            moveAt(e.pageX, e.pageY);
-        }
-        document.addEventListener('mousemove', onMouseMove);
-        draggedElement.onmouseup = function () {
-            document.removeEventListener('mousemove', onMouseMove);
-            draggedElement.onmouseup = null;
-            draggedElement.style.cursor = 'grab';
-        };
-    });
-    img.ondragstart = () => false;
+    img.addEventListener('mousedown', function (event) {
+    draggedElement = event.target;
+    offsetX = event.clientX - draggedElement.getBoundingClientRect().left;
+    offsetY = event.clientY - draggedElement.getBoundingClientRect().top;
+    draggedElement.style.position = 'absolute';
+    draggedElement.style.zIndex = 1000;
+    draggedElement.style.cursor = 'grabbing';
+    document.body.appendChild(draggedElement);
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 });
+    img.ondragstart = function () {
+    return false;
+};
+});
+    function onMouseMove(event) {
+    let posX = event.pageX - offsetX;
+    let posY = event.pageY - offsetY;
+    if (draggedElement) {
+    draggedElement.style.left = posX + 'px';
+    draggedElement.style.top = posY + 'px';
+}
+}
+    function onMouseUp() {
+    if (draggedElement) {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    draggedElement.style.cursor = 'grab';
+    draggedElement = null;
+}
+}
+
+
